a9e262583963809f7ea8fa0db3f838149a35917b
codebug8 <livelwh@outlook.com>
2022-01-12 06:24:35 +0000

    add tsv

--- board/st/stm32mp157/post-image.sh
+++ board/st/stm32mp157/post-image.sh
@@ -2,5 +2,8 @@
 set -e
 BOARD_DIR="$(dirname $0)"
 cp $BOARD_DIR/overlay/boot/* -rfd ${BINARIES_DIR}
+cp $BOARD_DIR/overlay/Ram  -rfd ${BINARIES_DIR}
+cp $BOARD_DIR/overlay/emmc_systemall.tsv  -rfd ${BINARIES_DIR}
+cp $BOARD_DIR/overlay/sdcard_systemall.tsv    -rfd ${BINARIES_DIR}
 
 exit $?
